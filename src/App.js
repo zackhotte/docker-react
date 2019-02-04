5da70535904c3a233eab5553a3ebf4d59f35dd7f
zackhotte <zack.hotte@gmail.com>
2019-02-04 18:51:16 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hello There!
+            Changed on feature branch!
           </a>
         </header>
       </div>
